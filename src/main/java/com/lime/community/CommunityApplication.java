1fe1dd155e03ca452e74f6e3662517adbcc3289b
jellmo <942560621@qq.com>
2022-02-19 22:25:44 +0800

    modify the application.yml

--- src/main/java/com/lime/community/CommunityApplication.java
+++ src/main/java/com/lime/community/CommunityApplication.java
@@ -1,5 +1,6 @@
 package com.lime.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author mohe
  */
 @SpringBootApplication
+@MapperScan("com.lime.community.repository")
 public class CommunityApplication {
 
     public static void main(String[] args) {
